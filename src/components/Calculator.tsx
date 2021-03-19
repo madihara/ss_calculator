02baebbad1d8c0202949c5d6749e776bb1b6575d
madihara <madisonhardine@gmail.com>
2021-03-19 12:18:00 -0400

    fixing font size issue

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Display from "./Display";
 
 const Calculator = () => {
     return (
-        <div className="w-2/5 bg-red-400 h-96 place-self-center">
-            Calculator Component
+        <div className="w-2/5 bg-gray-800 h-96 place-self-center">
+            <Display />
         </div>
     );
 };
---
+++ src/components/Display.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Display = () => {
+    return (
+        <div className="m-3 bg-gray-200 rounded-lg flex justify-end items-end p-2.5 text-gray-800  h-24">
+            <p className="text-4xl">100</p>
+        </div>
+    );
+};
+
+export default Display;
