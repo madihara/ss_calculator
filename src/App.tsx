7ecebf6fe9ce029f3362d25e2f1cc56fc4bdfe2c
madihara <madisonhardine@gmail.com>
2021-03-19 10:05:18 -0400

    setting up git

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function App() {
+    return (
+        <div className="font-mono bg-red-50  uppercase">
+            <header className="">
+                <a
+                    className=""
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Madi's calculator
+                </a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
