e3802f7a99e88b985d8ae63537cb78b48c579fb3
madihara <madisonhardine@gmail.com>
2021-03-22 09:20:54 -0400

    figured out first props interface

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import Display from "./Display";
+import NumberButton from "./NumberButton";
 
 const Calculator = () => {
     return (
-        <div className="w-2/5 bg-gray-800 h-96 place-self-center">
-            <Display />
-        </div>
+        <>
+            <div className="w-80 bg-gray-800 h-96 place-self-center">
+                <Display />
+                <div className="grid grid-cols-3">
+                    <NumberButton number="1" />
+                    <NumberButton number="2" />
+                    <NumberButton number="3" />
+                    <NumberButton number="4" />
+                </div>
+            </div>
+        </>
     );
 };
 
--- src/components/NumberButton.tsx
+++ src/components/NumberButton.tsx
@@ -1,1 +1,11 @@
-export {};
+import React from "react";
+
+interface Props {
+    number: string;
+}
+
+const NumberButton = (props: Props) => {
+    return <div className="bg-yellow-500 m-0.5 rounded-md">{props.number}</div>;
+};
+
+export default NumberButton;
